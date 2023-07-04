ed629522b56d04dbbe597335d7ea45f1543c434b
DevvIsme <mdatking123@gmail.com>
2023-07-04 11:21:00 +0700

    Thêm các thuộc tính màu mè

---
+++ lib/constrants.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class constyle {
+  static Color mauden = Color(0xFF141933);
+  static var outlineInputBorder = OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xFFDADEF2), width: 1.0),
+    borderRadius: BorderRadius.circular(10),
+  );
+  static Color mauxanh = Color(0xFF005FD0);
+  static Color mautrang = Color(0xFFF2F3F5);
+  static Color maunenlogin = Color(0xFF505673);
+}
