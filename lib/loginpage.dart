5b78261e6c07e1f53c76fd03d26a8ae5a084537b
DevvIsme <mdatking123@gmail.com>
2023-07-04 11:21:36 +0700

    Chỉnh sửa loginpage theo controller

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,33 +1,30 @@
 import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:crypto/crypto.dart';
 import 'package:http/http.dart';
 import 'package:newapp/appbar.dart';
-import 'package:newapp/btnmorelogin.dart';
-import 'package:newapp/forgetpass.dart';
-import 'package:newapp/information.dart';
-import 'package:newapp/newacc.dart';
-import 'package:newapp/textfieldusername.dart';
-import 'package:newapp/titleotherlogin.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:newapp/loginpage%20element/btnmorelogin.dart';
+import 'package:newapp/constrants.dart';
+import 'package:newapp/controller.dart';
+import 'package:newapp/loginpage%20element/forgetpass.dart';
+import 'package:newapp/loginpage%20element/newacc.dart';
+import 'package:newapp/loginpage%20element/titleotherlogin.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-var dulieu;
-var data;
-bool passwordvisible = true;
-
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController name = TextEditingController();
   TextEditingController pass = TextEditingController();
-
+  bool valipass = false;
+  bool valiname = false;
+  var data;
+  bool passwordvisible = true;
+  final _loginkey = GlobalKey<FormState>;
+  control ctl = control();
   void _showAlertDialog(BuildContext context, String stat, String prob) {
     showDialog(
       context: context,
@@ -46,75 +43,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 Text(stat),
                 if (stat == 'Đăng nhập Thất bại !!!') Text(prob)
               ],
-            )
-            );
+            ));
       },
     );
-  }
-
-  void login(String n, String p, BuildContext context) async {
-    //Response capcha = await
-    //Capcha();
-    setState(() {
-      Capcha();
-    });
-    try {
-      Response res = await post(
-        Uri.parse(
-            'http://lms-school-node1.vnpt.edu.vn/service/security/loginJWT'),
-        body: {'email': n, 'password': md5.convert(utf8.encode(p)).toString()},
-      );
-      // print(res.statusCode);
-      if (res.statusCode == 200) {
-        data = json.decode(res.body.toString());
-        //print(data);
-        //print(data['success']);
-        if (data['success'] == false) {
-          //print('Dang nhap that bai !!!');
-          _showAlertDialog(context, "Đăng nhập Thất bại !!!", data['msg']);
-          name.text = '';
-          pass.text = '';
-        } else {
-          //print('Dang nhap thanh cong !!!');
-          _showAlertDialog(context, "Đăng nhập Thành công !!!", "");
-          name.text = '';
-          pass.text = '';
-          SharedPreferences pref = await SharedPreferences.getInstance();
-          await pref.setString('datatoken', data['data']['token']);
-          //print(data['data']['token']);
-          print(data['data']['token']);
-          info(data['data']['token']);
-          /*  Navigator.push(context,
-            MaterialPageRoute(builder: (context) => InfoPage(tk: data['data']['token'])));*/
-        }
-      } else {
-        print('Extract Token: Failed');
-
-        data = json.decode(res.body.toString());
-        //print(data);
-      }
-    } catch (e) {
-      print('From get Token: ' + e.toString());
-    }
-  }
-
-  void info(var tk) async {
-    try {
-      Response res = await post(
-          Uri.parse(
-              'http://lms-school-node1.vnpt.edu.vn/service/user/getUserProfile'),
-          headers: <String, String>{'authorization': tk},
-          body: {});
-
-      if (res.statusCode == 200) {
-        dulieu = jsonDecode(res.body.toString());
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => InfoPage()));
-      } else
-        print('Extract Info: Failed');
-    } catch (e) {
-      print('From Gen INF: ' + e.toString());
-    }
   }
 
   Future<void> Capcha() async {
@@ -127,14 +58,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    //loaddata();
-
+    setState(() {
+      Capcha();
+    });
     print(data);
-    //setState(() { Capcha();});
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFFFFFFF),
-                elevation: 0,
+        backgroundColor: constyle.mautrang,
+        elevation: 0,
         title: appbar(),
         automaticallyImplyLeading: false,
         actions: <Widget>[
@@ -145,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Container(
-        color: Color(0xFFF2F3F5),
+        color: constyle.mautrang,
         child: ListView(
           children: [
             Padding(
@@ -154,80 +85,108 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
-                  //border: Border.all(color: Color(0xFF141933)),
-                  color: Colors.white,
+                  color: constyle.mautrang,
                 ),
                 child: Column(
-                  //mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                     Text(
+                    Text(
                       'Đăng nhập',
                       style: TextStyle(
                           fontSize: 30,
-                          color: Color(0xFF141933),
+                          color: constyle.mauden,
                           fontWeight: FontWeight.bold),
                     ),
                     Padding(
                       padding: EdgeInsets.all(20),
                       child: Container(
-                        //height: 250,
-                        child: Column(
-                          children: [
+                        child: Column(children: [
                           newacc(),
-                          SizedBox(height:10,),
-                          textfieldusername(name: name),
-                          SizedBox(height:30,),
-                           TextField(
-                          controller: pass,
-                          keyboardType: TextInputType.visiblePassword,
-                          obscureText: passwordvisible,
-                          style: TextStyle(
-                              color: Color(0xFF141933),
-                              fontWeight: FontWeight.bold),
-                          decoration: InputDecoration(
-                            hintText: 'Mật khẩu',
-                            suffixIcon: IconButton(
-                              icon: Icon(passwordvisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off),
-                              onPressed: () {
-                                setState(() {
-                                  passwordvisible = !passwordvisible;
-                                });
-                              },
-                            ),
-                            labelText: 'Mật khẩu',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Color(0xFFDADEF2), width: 1.0),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: Color(0xFFDADEF2), width: 1.0),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            /* border: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Color(0xFFDADEF2), width: 1.0),
-                              borderRadius: BorderRadius.circular(10),
-                            ),*/
+                          SizedBox(
+                            height: 10,
                           ),
-                        ),
+                          TextField(
+                            onChanged: (content) {
+                              setState(() {
+                                if (!name.text.isEmpty)
+                                  valiname = false;
+                                else
+                                  valiname = true;
+                              });
+                            },
+                            style: TextStyle(
+                                color: constyle.mauden,
+                                fontWeight: FontWeight.bold),
+                            controller: name,
+                            decoration: InputDecoration(
+                              labelText: 'Tên tài khoản',
+                              hintText: 'Tên tài khoản',
+                              errorText: valiname ? 'Hãy nhập mật khẩu' : null,
+                              enabledBorder: constyle.outlineInputBorder,
+                              focusedBorder: constyle.outlineInputBorder,
+                              border: constyle.outlineInputBorder,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            onChanged: (content) {
+                              setState(() {
+                                if (!pass.text.isEmpty)
+                                  valipass = false;
+                                else
+                                  valipass = true;
+                              });
+                            },
+                            controller: pass,
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: passwordvisible,
+                            style: TextStyle(
+                                color: constyle.mauden,
+                                fontWeight: FontWeight.bold),
+                            decoration: InputDecoration(
+                              errorText: valipass ? 'Hãy nhập mật khẩu' : null,
+                              hintText: 'Mật khẩu',
+                              suffixIcon: IconButton(
+                                icon: Icon(passwordvisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    passwordvisible = !passwordvisible;
+                                  });
+                                },
+                              ),
+                              labelText: 'Mật khẩu',
+                              enabledBorder: constyle.outlineInputBorder,
+                              focusedBorder: constyle.outlineInputBorder,
+                              border: constyle.outlineInputBorder,
+                            ),
+                          ),
                           forgetpass(),
                           Padding(
                             padding: EdgeInsets.only(bottom: 40),
                             child: ElevatedButton(
                                 style: ElevatedButton.styleFrom(
                                   padding: const EdgeInsets.all(15),
-                                  backgroundColor: Color(0xFF005FD0),
-                                  foregroundColor: Colors.white,
+                                  backgroundColor: constyle.mauxanh,
+                                  foregroundColor: constyle.mautrang,
                                   shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(10),
                                   ),
                                 ),
                                 onPressed: () {
-                                  login(name.text, pass.text, context);
+                                  setState(() {
+                                    pass.text.isEmpty
+                                        ? valipass = true
+                                        : valipass = false;
+                                    name.text.isEmpty
+                                        ? valiname = true
+                                        : valiname = false;
+                                  });
+                                  if (valiname == false && valipass == false) {
+                                    ctl.login(name, pass, context);
+                                  }
                                 },
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
@@ -254,4 +213,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
